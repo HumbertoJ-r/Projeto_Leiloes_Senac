d7afa7b0d2759bf664b61200a78b0a994f90a8ce
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2024-06-25 10:26:53 -0300

    Commit tela principal

---
+++ LeiloesTDSat/src/leiloes/main/TelaPrincipal.java
@@ -0,0 +1,11 @@
+package leiloes.main;
+
+import leiloes.view.cadastroVIEW;
+
+public class TelaPrincipal {
+
+    public static void main(String[] args) {
+        cadastroVIEW cv = new cadastroVIEW();
+        cv.setVisible(true);
+    }
+}
