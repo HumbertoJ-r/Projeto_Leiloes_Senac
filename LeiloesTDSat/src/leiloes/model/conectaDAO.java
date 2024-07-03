9d31138dbd751ee6eb40f44431cd2a7e62f36d3e
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2024-07-03 11:56:54 -0300

    #Commit Cadastrar

--- LeiloesTDSat/src/leiloes/model/conectaDAO.java
+++ LeiloesTDSat/src/leiloes/model/conectaDAO.java
@@ -16,7 +16,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "erick031119");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
